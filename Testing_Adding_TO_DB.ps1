65350d25c88faae5302796951e653614fd861f66
MSAdministrator <rickardj@missouri.edu>
2015-07-14 11:41:04 -0500

    update db code
    update db code

--- Testing_Adding_TO_DB.ps1
+++ Testing_Adding_TO_DB.ps1
@@ -1,5 +1,6 @@
 ﻿$cred = Get-Credential
 $username = 'unver_dv4'
+$PWord = ConvertTo-SecureString –String "password" –AsPlainText -Force
 $sqlobject = Get-VulnerableHosts -targetip "128.206.9.54,128.206.191.155,128.206.191.185" -QID "6" -credential $cred
 
 write-host $sqlobject
